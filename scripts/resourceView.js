c1762d3a76c4c812c35483137a3d2878dd43bd18
Aaron Filson <aaronfilson@gmail.com>
2015-12-22 13:48:04 -0800

    Database results need parsing.

---
+++ scripts/resourceView.js
@@ -0,0 +1,23 @@
+function compileTheProductivityTemplate(htmlTemplate){
+  var templateFunction = Handlebars.compile(htmlTemplate);
+  appendProductivityToPage(templateFunction);
+}
+
+function appendProductivityToPage(templateFunction){
+  //ask the database for all info on productivity
+  webDB.execute('SELECT * FROM resource WHERE contentPage = "productivity";', function(selectArray){
+    console.log('The selectArray variable is: ');
+    console.dir(selectArray);
+    //transform the data into what we need for the template
+    selectArray.rows.forEach(function(returnedResource){
+      $('#content').append(templateFunction(returnedResource));
+    });
+
+  });
+
+}
+
+function loadProductivityTemplate(){
+  $.get('/templates/productivityTemplate.html', compileTheProductivityTemplate)
+  .done(console.log('HELP'));
+}
