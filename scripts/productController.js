ba9df95b9296c1b7065304f320ebfdf4e83516d0
Yueyue07 <xww.qyy@gmail.com>
2015-12-23 13:17:30 -0800

    add the category filter function in productivity page

--- scripts/productController.js
+++ scripts/productController.js
@@ -3,3 +3,21 @@ var productController = {};
 productController.index = function() {
   product.loadAll(productView.index);
 };
+
+productController.category = function(ctx,next) {
+
+  console.log('run controller product');
+
+  var categoryData = function(products) {
+    ctx.products = products;
+    console.log('products' + ctx.products);
+    next();
+  };
+  product.findByCategory(ctx.params.category,categoryData);
+
+};
+
+
+productController.show = function(ctx) {
+  productView.show(ctx.products);
+};
